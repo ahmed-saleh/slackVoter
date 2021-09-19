c2c522f3e450b0c09f3f7744514c2d08ae574609
Ahmed Al-Salali <a.s.alsalali@gmail.com>
2021-09-19 22:55:45 +0800

    added entrypoint to handle composer installation

---
+++ entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+set -x
+
+echo ">> starting container as "
+
+# navigate to app path
+cd api
+
+cp .env.example .env
+
+VENDOR_DIR="/var/www/app/vendor"
+
+
+# Run composer install in case it was not installed
+if [[ ! -d $VENDOR_DIR ]]
+then
+    mkdir $VENDOR_DIR
+    composer install;
+fi
+
+# this is for development env Only
+php artisan migrate
+#php artisan key:generate #TODO: ensure correct permissions inside container before updating .env
+php artisan config:cache
+
+php artisan serve --host 0.0.0.0 --port 9000
+
+exec sudo "$@"
